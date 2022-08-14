27aa00f21dc4727c290c13e1a4e21585425741f1
Carlton Joseph <carlton.joseph@gmail.com>
2022-08-13 18:37:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Command Line Applications in Rust
+
+## Code History
+
+The code in this repository is based on the
+[Command Line Applications in Rust](https://rust-cli.github.io/book/index.html)
+online book.
+
+```bash
+cargo new grrs
+cd grrs/
+cargo run
+cargo add clap --features derive
+touch README.md
+```
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+use anyhow::{Context, Result};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.to_str().unwrap()))?;
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            let _ = writeln!(writer, "{}", line);
+        }
+    }
+}
+#[test]
+fn find_a_matches() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
